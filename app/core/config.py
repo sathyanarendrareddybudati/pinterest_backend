9a54c97cc108f88011fe9f43150e7074242cde32
sathyanarendrareddybudati <sathyanarendrareddybudati@gmail.com>
2026-05-31 01:05:27 +0200

    updated code

--- app/core/config.py
+++ app/core/config.py
@@ -2,15 +2,18 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://postgres:Satya199$$@localhost/pinterest"
-    REDIS_URL: str = "redis://localhost:6379/0"
-    ELASTICSEARCH_URL: str = "http://localhost:9200"
-    ELASTICSEARCH_INDEX: str = "pins"
-    SECRET_KEY: str = "b1e2f3g4h5i6j7k8l9m0n1o2p3q4r5s6t7u8v9w0x1y2z3a4"
-    ALGORITHM: str = "HS256"
-    CLOUDINARY_CLOUD_NAME: str = "dgsyfonz8"
-    CLOUDINARY_API_KEY: str = "226568586269397"
-    CLOUDINARY_API_SECRET: str = "8I7jR_F2RAzWde1yeTENfZ9-anM"
+    DATABASE_URL: str
+    REDIS_URL: str
+    ELASTICSEARCH_URL: str
+    ELASTICSEARCH_INDEX: str
+    SECRET_KEY: str
+    ALGORITHM: str
+    CLOUDINARY_CLOUD_NAME: str
+    CLOUDINARY_API_KEY: str
+    CLOUDINARY_API_SECRET: str
+
+    class Config:
+        env_file = ".env"
 
 
 settings = Settings()
